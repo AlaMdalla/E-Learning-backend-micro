9ccc9a76c4ed345cf357cf54a3cfb857e11ad8f5
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2025-02-08 14:01:51 +0000

    add spring-cloud.version

--- microservices/Problems/Problems/src/main/java/E_Learning/Project/ProjectApplication.java
+++ microservices/Problems/Problems/src/main/java/E_Learning/Project/ProjectApplication.java
@@ -1,8 +1,11 @@
 package E_Learning.Project;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProjectApplication {
 

f8c8ec825c83eea14462430e39e99e64e77c545b
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2025-02-28 17:05:02 +0000

    added update to competion

--- Solution.java
+++ Solution.java
@@ -1,22 +1,5 @@
 import java.util.*;
-public class Solution {public static int[] twoSum(int[] nums, int target) {
-            int[] returnsum =new int[2];
-            int n =nums.length;
-            Map<Integer,Integer> targetMap=new HashMap<>();
-
-            for (int i=0;i<n;i++){
-                int targ0=target-nums[i];
-                if (targetMap.get(targ0)!=null)
-                {
-  returnsum[1]=i;
-                    int val=targetMap.get(targ0);
-                    returnsum[0]=targetMap.get(targ0);
-                    return returnsum;
-                }
-  targetMap.put(nums[i+1],i);
-            }
-return null;
-            }public static void main(String[] args) {
+public class Solution {épublic static void main(String[] args) {
     int target = Integer.parseInt(args[1]);
     String[] strNumbers = args[0].split(" ");
     int[] nums = new int[strNumbers.length];

b56b8e51410d5052eb8fdaf3c2523a029161b399
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2025-04-13 18:33:54 +0100

    merge subs

--- microservices/subscription/src/main/java/esprit/subscription/Controller/SubsController.java
+++ microservices/subscription/src/main/java/esprit/subscription/Controller/SubsController.java
@@ -15,34 +15,35 @@ public class SubsController {
     @Autowired
     private SubsService subsService;
 
-    // Create (Add a new subscription)
     @PostMapping("/add")
     public ResponseEntity<Subs> addNewSubs(@RequestBody Subs subs) {
         Subs createdSubs = subsService.addNewSubs(subs);
         return ResponseEntity.ok(createdSubs);
     }
 
-    // Read (Get all subscriptions)
     @GetMapping("/all")
     public ResponseEntity<List<Subs>> getAllSubs() {
         return ResponseEntity.ok(subsService.getAllSubs());
     }
 
-    // Read (Get a subscription by ID)
     @GetMapping("/{id}")
     public ResponseEntity<Subs> getSubsById(@PathVariable Integer id) {
         Subs subs = subsService.getSubsById(id);
         return subs != null ? ResponseEntity.ok(subs) : ResponseEntity.notFound().build();
     }
 
-    // Update (Modify an existing subscription)
+    @GetMapping("/status/{id}")
+    public ResponseEntity<String> getSubsStatus(@PathVariable Integer id) {
+        Subs subs = subsService.getSubsById(id);
+        return subs != null ? ResponseEntity.ok(subs.getStatus()) : ResponseEntity.notFound().build();
+    }
+
     @PutMapping("/update/{id}")
     public ResponseEntity<Subs> updateSubs(@PathVariable Integer id, @RequestBody Subs newSubsData) {
         Subs updatedSubs = subsService.updateSubs(id, newSubsData);
         return updatedSubs != null ? ResponseEntity.ok(updatedSubs) : ResponseEntity.notFound().build();
     }
 
-    // Delete (Remove a subscription by ID)
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteSubs(@PathVariable Integer id) {
         boolean isDeleted = subsService.deleteSubs(id);
